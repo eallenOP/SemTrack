6fc3720e0ea8bc629ca2847cbf4a5b56acf04f1e
Elise <elise.allen@op.ac.nz>
2017-09-22 17:44:00 +1200

    Still not working

--- editSem.php
+++ editSem.php
@@ -14,33 +14,38 @@ editSemForm.html.php
 include 'inc/connect.inc.php'; //connection details (keep secure)
 
 try {
-    //Get the right ID from URL
-    $semID = $_GET['id'];
-
+    
     //If submit clicked, update the database with form contents
     if (isset($_POST['editSem'])) {
+        $ID = $_POST['id']; 
         $sql = "UPDATE Semester
                 SET 'semNum' = :num,
                     'startDate' = :start,
                     'endDate' = :end,
                     'breakStart' = :breakStart,
                     'breakEnd' = :breakEnd
-                WHERE 'semID = $semID";
+                WHERE 'semID = $ID";
+                include 'semDates.php';
+    } else {
+
+        //Get the right ID from URL
+        $semID = $_GET['id'];
+    
+        //Retrieve information from database
+        $sql = "SELECT * FROM Semester WHERE semID = $semID";
+        $result = $pdo->query($sql);
+        $result = $result->fetch();
+    
+        //Populate the edit form with the current details
+        $semNum = $result['semNum'];
+        $start = $result['startDate'];
+        $end = $result['endDate'];
+        $breakStart = $result['holStart'];
+        $breakEnd = $result['holEnd'];
+    
+        include 'editSemForm.html.php';
     }
-
-    //Retrieve information from database
-    $sql = "SELECT * FROM Semester WHERE semID = $semID";
-    $result = $pdo->query($sql);
-    $result = $result->fetch();
-
-    //Populate the edit form with the current details
-    $semNum = $result['semNum'];
-    $start = $result['startDate'];
-    $end = $result['endDate'];
-    $breakStart = $result['holStart'];
-    $breakEnd = $result['holEnd'];
-
-    include 'editSemForm.html.php';
+    
 
 } catch (PDOException $e) {
     $error = 'Something failed';
